920eef3621d6e4113086a851449c1ec5edb5cb33
Queen <qogheneochuko@gmail.com>
2024-10-10 16:58:38 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""To open all boxes"""
+
+def canUnlockAll(boxes):
+    """houses the function"""
+    num = len(boxes)
+    opened = set([0])
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key < num and key not in opened:
+                opened.add(key)
+                stack.append(key)
+    
+    return len(opened) == num
+
