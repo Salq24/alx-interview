e8cf9b924175b3bef90843df5bc8fffa1ae71159
Salq24 <142992679+Salq24@users.noreply.github.com>
2024-10-20 21:26:02 +0100

    add

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
+
 """
-A method that calculates the fewest number of 
-operations needed to result in exactly n 'H' characters in a file.
+    a method that calculates the fewest number of 
+    operations needed to result in exactly n H characters in the file
 """
 
-
-def min_operations(n):
+def minOperations(n):
     """
-    This function calculates the fewest number of operations
-    needed to get exactly n 'H' characters in a file.
-
-    :param n: The number of 'H' characters desired.
-    :return: The minimum number of operations.
+    This function calcs the fewest number of ops
+    needed to get a result of exactly nH xters in
+    a file
     """
-
-    current_chars = 1  # Starting with 1 'H'
-    last_copied = 0    # Number of characters last copied
-    operations = 0     # Operation count
-
-    if n <= 1:
-        return 0  # No operations needed if n <= 1
-    
-    while current_chars < n:
-        # If the remaining characters (n - current_chars) is divisible
-        # by the current number of characters, we should copy and then paste.
-        if (n - current_chars) % current_chars == 0:
-            last_copied = current_chars
-            operations += 1  # Copy operation
-        # Paste operation
-        current_chars += last_copied
-        operations += 1
-
-    return operations
+    one = 1
+    start = 0
+    cnt = 0
+    while one < n:
+        remain = n - one
+        if (remain % one == 0):
+            start = one
+            one += start
+            cnt += 2
+        else:
+            one += start
+            cnt += 1
+    return cnt
