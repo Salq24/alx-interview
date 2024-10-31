3b7e8d409156fca85e4e40ae89d8462e212097bf
Salq24 <142992679+Salq24@users.noreply.github.com>
2024-10-31 22:35:00 +0000

    utf8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""determines if a given data set represents a valid UTF-8 encoding."""
+
+
+def validUTF8(data):
+    """utf-8 validation"""
+
+    n_bytes = 0
+    
+    test_1 = 1 << 7
+    test_2 = 1 << 6
+
+    for i in data:
+        mask = 1 << 7
+        if n_bytes == 0:
+            while mask & i :
+                n_bytes += 1
+                mask = mask >> 1
+            if n_bytes == 0:
+                continue
+
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+            if not (i & test_1 and not (i & test_2)):
+                return False
+            
+        n_bytes -= 1
+
+    if n_bytes == 0:
+        return True
+    
+    return False
